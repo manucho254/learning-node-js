18442075655adaea6b8bcf3ef742e114876df8c9
Emmanuel manucho <manuchoadero@gmail.com>
2023-02-15 21:33:52 +0300

    jobs-api

--- jobs-api/starter/app.js
+++ jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packages
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -17,10 +24,25 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// security middleware
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // app routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/jobs", userAuthenticated, jobRoutes);
 
+//
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
